de9f1cd1a22658b2267df5c41951d8d437b56394
Angel <angel92.addcs@gmail.com>
2023-09-30 22:57:49 -0500

    working game

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,71 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Card from './components/card.jsx'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function shuffle(array) {
+  var index = array.length;
+  var number = 0;
+  var temp;
 
-  const children = []
-  for(let i = 0; i < 20; i++) {
-    children.push(<Card id={i+1}/>);
+  while (index--) {
+      number = Math.floor(Math.random() * (index+1));
+      temp = array[index];
+      array[index] = array[number];
+      array[number] = temp;
+  }
+  return array;
+}
+
+const pairs = shuffle([1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10])
+function App() {
+  const [points, setPoints] = useState(0);
+  const [errors, setErrors] = useState(0);
+
+  var last = undefined
+
+  const spin = function(event, pair, id)
+  {
+    var card = document.getElementById(id)
+    if (card.classList.contains("correct")) return
+
+    var showing = document.getElementsByClassName("show");
+    if (last == undefined) {
+      last = {
+        card: card,
+        pair: pair,
+      }
+      card.classList.add("show")
+      return
+    }
+    
+    card.classList.add("show")
+    if (last.pair == pair) {
+      last.card.classList.add("correct")
+      card.classList.add("correct")
+      setPoints(points + 5)
+      last = undefined
+    } else {
+      setTimeout(() => {
+        console.log("he")
+        card.classList.remove("show")
+        last.card.classList.remove("show")
+        last = undefined
+        setErrors(errors + 1)
+      }, 500)
+    }
   }
 
   return (
     <>
-      {children}
+      <div className='points'>
+        <p>Points: {points}</p>
+        <p>Fails: {errors}</p>
+      </div>
+      <div className='cards'>
+        {pairs.map((id,index) => (
+          <Card key={index} pair={id} onClick={spin}/>
+        ))} 
+      </div>
     </>
   )
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,17 @@
+import { useId } from 'react';
+
 export default function(props) {
-    return <div>
-        <p>card {props.id}</p>
+    
+    const current = useId()
+    const onClick = function(event) {
+        if (props.onClick) props.onClick(event, props.pair, current)
+    }
+
+    return <div id={current} className="card" onClick={onClick}>
+        <div className="face front">
+            <p>{props.pair}</p>
+        </div>
+        <div className="face back">
+        </div>
     </div>
 }
